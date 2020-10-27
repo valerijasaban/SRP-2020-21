baa81407db5b1dfd675f0971402924409e685108
Valerija_Saban <vsaban01@fesb.hr>
2020-10-27 19:23:13 +0100

    test message protection/authentication

--- postgres-express-node/index.js
+++ postgres-express-node/index.js
@@ -114,7 +114,7 @@ async function init() {
     // * ===============================
     // * Example: Authenticating a file
     // *--------------------------------
-    const CREATE_TAG = false;
+    /*const CREATE_TAG = false;
 
     if (CREATE_TAG) {
       // * Authenticate the file
@@ -144,7 +144,7 @@ async function init() {
           } authentic.`
         );
       });
-    }
+    } */
   } catch (err) {
     console.log(err.message);
   }
